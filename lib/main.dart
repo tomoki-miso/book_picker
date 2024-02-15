398dc800eb628f3d616cbe91eafe3ef00980f285
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-02-15 10:31:05 +0900

    scopeの設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,41 @@
+import 'package:book_picker/features/top/page.dart';
 import 'package:book_picker/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        home: MainApp(),
+      ),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+  Widget build(BuildContext context) => MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: ElevatedButton(
+              onPressed: () async => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const TopPage(),
+                ),
+              ),
+              child: const Text('hello'),
+            ),
+          ),
         ),
-      ),
-    );
-  }
+      );
 }
