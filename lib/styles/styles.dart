5b0a7430e25d2d4e308854411f9952983e038f87
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-02-16 14:21:36 +0900

    style

---
+++ lib/styles/styles.dart
@@ -0,0 +1,39 @@
+import 'package:book_picker/styles/colors.dart';
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const bookTitleStyle = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: ColorName.blackBase,
+    overflow: TextOverflow.ellipsis,
+  );
+
+  static const bookAuthorStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: ColorName.blackBase,
+    overflow: TextOverflow.ellipsis,
+  );
+
+  static const defaultStyle = TextStyle(
+    fontSize: 12,
+    color: ColorName.blackBase,
+    overflow: TextOverflow.ellipsis,
+  );
+
+  static const topCaptionStyle = TextStyle(
+    fontSize: 12,
+    color: ColorName.blackBase,
+    overflow: TextOverflow.ellipsis,
+  );
+
+  static const greyDefaultStyle = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+    color: ColorName.greyBase,
+  );
+}
+
+const double kDefaultPadding = 16;
+const double kDefaultSize = 4;
