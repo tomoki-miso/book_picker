61e20c3bb213655e8b3b8f538bd6ac0011728463
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-02-16 14:19:21 +0900

    背景

---
+++ lib/components/gradation_container.dart
@@ -0,0 +1,25 @@
+import 'package:book_picker/styles/colors.dart';
+import 'package:flutter/material.dart';
+
+class GradatioonContainer extends StatelessWidget {
+  const GradatioonContainer({
+    required this.child,
+    super.key,
+  });
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) => DecoratedBox(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: <Color>[
+              ColorName.gradationStart,
+              ColorName.gradationEnd,
+            ],
+          ),
+        ),
+        child: child,
+      );
+}
