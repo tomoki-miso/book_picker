885e81fbea621a09ad496493b59f50283bbd99ef
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-02-18 21:51:39 +0900

    backGround

---
+++ lib/components/back_ground.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BackGround extends StatelessWidget {
+  const BackGround({
+    required this.child,
+    super.key,
+  });
+
+  final Widget child;
+  @override
+  Widget build(BuildContext context) => Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/back_ground_image.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: child,
+      );
+}
