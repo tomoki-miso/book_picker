054317f623308dae476e1859a3928cfa33e36e4b
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-02-18 21:55:14 +0900

    bookInfoTile設置　

--- lib/features/book_info/page.dart
+++ lib/features/book_info/page.dart
@@ -1,11 +1,14 @@
+import 'package:book_picker/components/back_ground.dart';
+import 'package:book_picker/components/book_info_tile.dart';
+import 'package:book_picker/components/original_app_bar.dart';
 import 'package:book_picker/domain/common_storing_book/domain.dart';
 import 'package:book_picker/domain/fetched_book/domain.dart';
 import 'package:book_picker/domain/user_storing_book/domain.dart';
 import 'package:book_picker/features/book_info/page_type.dart';
 import 'package:book_picker/features/book_info/view_model.dart';
+import 'package:book_picker/styles/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class BookInfoPage extends ConsumerWidget {
   const BookInfoPage({
@@ -15,12 +18,18 @@ class BookInfoPage extends ConsumerWidget {
     this.userStoringBook,
     super.key,
   });
-  static const rootName = '/info';
 
   final PageType pageType;
   final FetchedBook? fetchedBook;
   final CommonStoringBook? commonStoringBook;
   final UserStoringBook? userStoringBook;
+
+  // void switch(pageType) async {
+  //   case 'todaysBook';
+  //   final BookInfo=fetchedBook;
+  //   return BookInfo;
+
+  // }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -33,21 +42,21 @@ class BookInfoPage extends ConsumerWidget {
       ),
     );
     return state.when(
-      data: (data) => Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          actions: [
-            ElevatedButton(
-              onPressed: () {
-                context.pop();
-              },
-              child: const Text('back'),
-            ),
-          ],
-        ),
-        body: const Center(
-          child: Text('infomation'),
-        ),
+      data: (data) => const Scaffold(
+        appBar: OriginalAppBar(),
+        body: BackGround(
+            child: Padding(
+          padding: EdgeInsets.all(kDefaultPadding),
+          child: BookInfoTile(
+            title: 'aaaa',
+            author: 'aaa',
+            imageUrl:
+                'https://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/5010/9784101005010_1_2.jpg?_ex=200x200',
+            isbn: '1111111111',
+            itemPrice: '2222',
+            itemCaption: 'aaaaa',
+          ),
+        )),
       ),
       error: (error, stackTrace) => ElevatedButton(
         onPressed: () {
