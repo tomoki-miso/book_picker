21155a527eb2055e8c91d8d33bf5b7cf26ce1b46
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-02-18 21:57:40 +0900

    firebase関連削除

--- lib/repository/fetched_book/repository.dart
+++ lib/repository/fetched_book/repository.dart
@@ -1,38 +1,16 @@
 import 'package:book_picker/domain/fetched_book/domain.dart';
 import 'package:book_picker/env/env.dart';
-import 'package:book_picker/firebase/firebase_provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'repository.g.dart';
 
 @Riverpod(keepAlive: true)
-CollectionReference<FetchedBook> fetchedBookFirestore(
-  FetchedBookFirestoreRef ref,
-) =>
-    ref
-        .read(firestoreProvider)
-        .collection('user')
-        .doc('Rrhwx2cGWjGWjNg7IcsY') // TODO:カレントユーザーに変更
-        .collection('user_todays_book')
-        .withConverter<FetchedBook>(
-          fromFirestore: (snapshot, _) =>
-              FetchedBook.fromJsonStore(snapshot.data()!),
-          toFirestore: (data, _) => data.toJson(),
-        );
-
-@Riverpod(keepAlive: true)
 class FetchedBookRepo extends _$FetchedBookRepo {
-  FirebaseFirestore get db => ref.read(firestoreProvider);
-
-  CollectionReference<FetchedBook> get collection =>
-      ref.read(fetchedBookFirestoreProvider);
-
   @override
   void build() {}
 
-  /// APIから本を取り出す
+  /// APIから取り出す
   Future<FetchedBook> fetchBookInfo(String isbn) async {
     final dio = Dio();
     final String appId = Env.key;
@@ -42,20 +20,14 @@ class FetchedBookRepo extends _$FetchedBookRepo {
     final url = Uri.parse(
       '$apiUrl?format=json&isbnjan=$isbn&applicationId=$appId',
     );
-    print(url);
     final response = await dio.getUri(url);
 
     try {
       final data = response.data;
-      print(data);
       final FetchedBook book = FetchedBook.fromJson(data);
       return book;
     } catch (e) {
       throw e;
     }
-  }
-
-  Future<void> setTodaysBook(FetchedBook fetchedBook) async {
-    await collection.doc('1').set(fetchedBook);
   }
 }
--- lib/repository/fetched_book/repository.g.dart
+++ lib/repository/fetched_book/repository.g.dart
@@ -6,24 +6,7 @@ part of 'repository.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$fetchedBookFirestoreHash() =>
-    r'f7c0482a1627a3ce95cbf313d8367e826e992608';
-
-/// See also [fetchedBookFirestore].
-@ProviderFor(fetchedBookFirestore)
-final fetchedBookFirestoreProvider =
-    Provider<CollectionReference<FetchedBook>>.internal(
-  fetchedBookFirestore,
-  name: r'fetchedBookFirestoreProvider',
-  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-      ? null
-      : _$fetchedBookFirestoreHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
-
-typedef FetchedBookFirestoreRef = ProviderRef<CollectionReference<FetchedBook>>;
-String _$fetchedBookRepoHash() => r'd93cf95bd5c9aa9be7132a0b7532d4792d371984';
+String _$fetchedBookRepoHash() => r'80e14fe567980571ce219b35b1c76c62525bff0a';
 
 /// See also [FetchedBookRepo].
 @ProviderFor(FetchedBookRepo)
