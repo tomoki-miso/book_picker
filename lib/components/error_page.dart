c152fc6d6e9d1d64440788644fa279619fd51d44
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-02-20 12:58:46 +0900

    エラーページアニメーション追加

--- lib/components/error_page.dart
+++ lib/components/error_page.dart
@@ -1,6 +1,7 @@
 import 'package:book_picker/components/back_ground.dart';
 import 'package:book_picker/styles/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class ErrorPage extends StatelessWidget {
   const ErrorPage({required this.error, super.key});
@@ -8,25 +9,29 @@ class ErrorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         body: BackGround(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'エラーが発生しました。',
-              style: Styles.bookTitleStyle,
-            ),
-            const SizedBox(
-              height: kDefaultPadding,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                  height: 400,
+                  child: Lottie.asset('assets/lottie/error_cat.json')),
+              const Text(
+                'エラーが発生しました。',
+                style: Styles.bookTitleStyle,
+              ),
+              const SizedBox(
+                height: kDefaultPadding,
+              ),
 
-            /// TODO:リリース前に削除
-            ElevatedButton(
-              onPressed: () {
-                print(error);
-              },
-              child: const Text('エラーチェック'),
-            ),
-          ],
-        )),
+              /// TODO:リリース前に削除
+              ElevatedButton(
+                onPressed: () {
+                  print(error);
+                },
+                child: const Text('エラーチェック'),
+              ),
+            ],
+          ),
+        ),
       );
 }
