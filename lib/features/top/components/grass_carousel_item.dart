842c93e2621347a825246c4907d9e3ab3932ed5f
tomoki-miso <u112354e@ecs.osaka-u.ac.jp>
2024-02-20 13:01:53 +0900

    カルーセルボタン追加

--- lib/features/top/components/grass_carousel_item.dart
+++ lib/features/top/components/grass_carousel_item.dart
@@ -1,47 +1,51 @@
 import 'package:book_picker/components/grass_container.dart';
+import 'package:book_picker/domain/book/domain.dart';
+import 'package:book_picker/features/book_info/page.dart';
 import 'package:book_picker/styles/styles.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class GrassCarouselItem extends StatelessWidget {
   const GrassCarouselItem({
-    this.title,
-    this.author,
-    this.imageUrl,
+    required this.book,
     super.key,
   });
 
-  final String? title;
-  final String? author;
-  final String? imageUrl;
+  final Book book;
 
   @override
-  Widget build(BuildContext context) => GrassContainer(
-        width: 0.4,
-        child: Column(
-          children: [
-            const SizedBox(
-              height: kDefaultSize * 2,
-            ),
-            Text(
-              title ?? '',
-              style: Styles.defaultStyle,
-            ),
-            const SizedBox(
-              height: kDefaultSize * 2,
-            ),
-            Text(
-              author ?? '',
-              style: Styles.greyDefaultBoldStyle,
-            ),
-            const SizedBox(
-              height: kDefaultSize * 2,
-            ),
-            CachedNetworkImage(
-              height: MediaQuery.of(context).size.height * 0.17,
-              imageUrl: imageUrl ?? '',
-            ),
-          ],
+  Widget build(BuildContext context) => GestureDetector(
+        onTap: () async => Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => BookInfoPage(book: book)),
+        ),
+        child: GrassContainer(
+          width: 0.4,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: kDefaultSize * 2,
+              ),
+              Text(
+                book.title ?? '',
+                style: Styles.defaultStyle,
+              ),
+              const SizedBox(
+                height: kDefaultSize * 2,
+              ),
+              Text(
+                book.author ?? '',
+                style: Styles.greyDefaultBoldStyle,
+              ),
+              const SizedBox(
+                height: kDefaultSize * 2,
+              ),
+              CachedNetworkImage(
+                height: MediaQuery.of(context).size.height * 0.17,
+                imageUrl: book.imageUrl ?? '',
+              ),
+            ],
+          ),
         ),
       );
 }
